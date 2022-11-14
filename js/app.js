f6bf7d6e45afb52b1c0572adb9bdeb8a0a27c383
Vladyslav Ryzhykov <ryzhykovvv@gmail.com>
2022-11-14 13:46:35 +0300

    made changes (version 2)

--- js/app.js
+++
@@ -1,7 +0,0 @@
-import {slider} from './slider.js';
-import {burger} from './burger.js';
-import {accordion} from './accordion.js';
-import {choices} from './choices.js';
-import {slowShow} from './slow-show.js';
-
-
